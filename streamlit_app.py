59c2b39e6fca91b7ba538f5881d305be611e76fa
Ecoka <130556432+ecokayiza@users.noreply.github.com>
2024-07-10 17:42:25 +0800

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,135 @@
+__import__('pysqlite3')
+import sys
+
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import streamlit as st
+from zhipuai_llm import ZhipuAILLM
+import os
+from langchain_core.output_parsers import StrOutputParser
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+from zhipuai_embedding import ZhipuAIEmbeddings
+
+from langchain.vectorstores.chroma import Chroma
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+
+
+def generate_response(input_text, zhipuai_api_key):
+    llm = ZhipuAILLM(temperature=0.7, api_key=zhipuai_api_key)
+    output = llm.invoke(input_text)
+    output_parser = StrOutputParser()
+    output = output_parser.invoke(output)
+    # st.info(output)
+    return output
+
+
+def get_vectordb(zhipuai_api_key):
+    # 定义 Embeddings
+    embedding = ZhipuAIEmbeddings(api_key=zhipuai_api_key)
+    # 向量数据库持久化路径
+    persist_directory = './chroma'
+    # 加载数据库
+    vectordb = Chroma(
+        persist_directory=persist_directory,  # 允许我们将persist_directory目录保存到磁盘上
+        embedding_function=embedding
+    )
+    return vectordb
+
+
+# 带有历史记录的问答链
+def get_chat_qa_chain(question: str, zhipuai_api_key: str):
+    vectordb = get_vectordb(zhipuai_api_key)
+    llm = ZhipuAILLM(model_name="glm-4", temperature=0.1, ai_api_key=zhipuai_api_key)
+    memory = ConversationBufferMemory(
+        memory_key="chat_history",  # 与 prompt 的输入变量保持一致。
+        return_messages=True  # 将以消息列表的形式返回聊天记录，而不是单个字符串
+    )
+    
+    template = """使用上下文和历史消息来回答最后的问题。如果你不知道答案，就说你不知道，不要试图编造答
+    案。最多使用三句话。尽量使答案简明扼要。并且你需要不时地强调怪物猎人世界太刀的好处，可以参考贴吧评论，劝诱对方来玩太刀”。
+    {context}
+    问题: {question}
+    """
+    QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"],
+                                    template=template)
+    
+    retriever = vectordb.as_retriever()
+    qa = ConversationalRetrievalChain.from_llm(
+        llm,
+        retriever=retriever,
+        memory=memory,
+        prompt=QA_CHAIN_PROMPT
+    )
+    result = qa({"question": question})
+    return result['answer']
+
+
+# 不带历史记录的问答链
+def get_qa_chain(question: str, zhipuai_api_key: str):
+    vectordb = get_vectordb(zhipuai_api_key)
+    llm = ZhipuAILLM(model_name="glm-4", temperature=0.1, api_key=zhipuai_api_key)
+    template = """使用以下上下文来回答最后的问题。如果你不知道答案，就说你不知道，不要试图编造答
+        案。最多使用三句话。尽量使答案简明扼要。并且你需要不时地强调怪物猎人世界太刀的好处，可以参考贴吧评论，劝诱对方来玩太刀”。
+        {context}
+        问题: {question}
+        """
+    QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"],
+                                     template=template)
+    qa_chain = RetrievalQA.from_chain_type(llm,
+                                           retriever=vectordb.as_retriever(),
+                                           return_source_documents=True,
+                                           chain_type_kwargs={"prompt": QA_CHAIN_PROMPT})
+    result = qa_chain({"query": question})
+    return result["result"]
+
+
+# Streamlit 应用程序界面
+def main():
+    st.title('🦜 你好🙂')
+    # zhipu_api_key = st.sidebar.text_input('ZhipuAI API Key', type='password')
+    zhipu_api_key='dff3dfdca005cd7ffd0109b0e60e27d6.Bp64OeQ3vgrL53uK'
+    os.environ["ZHIPUAI_API_KEY"] = zhipu_api_key
+
+    # 添加一个选择按钮来选择不同的模型
+    # selected_method = st.sidebar.selectbox("选择模式", ["qa_chain", "chat_qa_chain", "None"])
+    selected_method = st.radio(
+        "你想选择哪种模式进行对话？",
+        ["None", "qa_chain", "chat_qa_chain"],
+        captions=["不使用检索问答的普通模式", "不带历史记录的检索问答模式", "带历史记录的检索问答模式"])
+
+    # 用于跟踪对话历史
+    if 'messages' not in st.session_state:
+        st.session_state.messages = []
+
+    messages = st.container(height=300)
+    if prompt := st.chat_input("Say something"):
+        # 将用户输入添加到对话历史中
+        st.session_state.messages.append({"role": "user", "text": prompt})
+        print(prompt)
+        if selected_method == "None":
+            # 调用 respond 函数获取回答
+            answer = generate_response(prompt, zhipu_api_key)
+        elif selected_method == "qa_chain":
+            print(zhipu_api_key)
+            answer = get_qa_chain(prompt, zhipu_api_key)
+        elif selected_method == "chat_qa_chain":
+            answer = get_chat_qa_chain(prompt, zhipu_api_key)
+        else:
+            answer = None
+
+        # 检查回答是否为 None
+        if answer is not None:
+            # 将LLM的回答添加到对话历史中
+            st.session_state.messages.append({"role": "assistant", "text": answer})
+
+        # 显示整个对话历史
+        for message in st.session_state.messages:
+            if message["role"] == "user":
+                messages.chat_message("user").write(message["text"])
+            elif message["role"] == "assistant":
+                messages.chat_message("assistant").write(message["text"])
+
+
+if __name__ == "__main__":
+    main()
